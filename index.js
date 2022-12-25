c6dc7e0f828faf5af1bc75bc4071cbfb896d0c75
Quentin Neal <quentinneal@outlook.com>
2022-12-25 00:30:21 -0800

    Express adjustment.

--- index.js
+++ index.js
@@ -8,17 +8,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-
-  app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-  
-}
-
-
-
 app.get("/rooms_list", async (req, res) => {
   try {
     const roomsList = await pool.query("SELECT * FROM room_list");
@@ -27,6 +16,16 @@ app.get("/rooms_list", async (req, res) => {
     console.error(err.message);
   }
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
