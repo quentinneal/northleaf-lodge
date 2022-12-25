c259ff26b00ae9ca40f9f3dcf982fd11bebe1423
Quentin Neal <quentinneal@outlook.com>
2022-12-25 00:08:18 -0800

    Express adjustment.

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
+  
 }
+
+
 
 app.get("/rooms_list", async (req, res) => {
   try {
